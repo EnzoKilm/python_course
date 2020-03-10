ac83d6be8663b0a91a9ea08c30c41b6c3a2ab18b
EnzoKilm <enzo-beauchamp@live.fr>
2020-03-10 12:21:45 +0100

    Cours 10/03 matin

---
+++ exo2 - fonctions.py
@@ -0,0 +1,20 @@
+# Créer une fonction 1
+def nom_de_ma_fonction():
+    print(15)
+
+nom_de_ma_fonction()
+
+# Créer une fonction 2
+def fonction_age():
+    age = int(input("Quel est votre age ?\n")
+    return age
+
+fonction_age()
+
+# Créer une fonction avec un argument
+mon_age = 36
+def qui_je_suis(age):
+    prenom = input("Votre prénom")
+
+
+qui_je_suis(mon_age)
