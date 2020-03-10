8371cfd1581611a9a5e3ed8400bd9c7f00c6a4f5
EnzoKilm <enzo-beauchamp@live.fr>
2020-03-10 16:24:06 +0100

    Modifs exo Rico

--- exo4 - argent (rico).py
+++ exo4 - argent (rico).py
@@ -5,27 +5,25 @@ argent = 1000
 # si je gagne je double ma mise si je perd ba je perd
 
 while argent > 0:
-    texte_argent = "║Solde : " + str(argent) + "€ ║"
+    texte_argent = "║ Solde : " + str(argent) + "€ ║"
     barre = ""
     for i in range(0, len(texte_argent)-2):
         barre += "═"
-    print("╔"+barre+"╗")
-    print(texte_argent)
-    print("╚"+barre+"╝")
+    print("╔"+barre+"╗\n"+texte_argent+"\n╚"+barre+"╝")
     
     try:
-        mise = int(input("Combien misez vous ? : \n"))
+        mise = int(input("Votre mise : "))
 
         if mise > argent:
-            print("vous n'avez pas assez d'argent")
+            print("Vous n'avez pas assez d'argent!")
         else :
             lance = random()
             if lance < 0.5 :
                 argent += mise
-                print("vous avez gagné",mise*2)
+                print("Vous avez gagné",mise,"€")
             else :
                 argent -= mise
-                print("vous avez perdu",mise)
+                print("Vous avez perdu",mise,"€")
 
     except:
         print("Merci d'avoir joué, à bientôt!")
